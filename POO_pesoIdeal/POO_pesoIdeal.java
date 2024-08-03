ecbd9efada82c7c51f31bd87bcc20094c51b80ac
nadiabertoldos <nadiabertoldoz@gmail.com>
2024-08-03 01:01:31 -0300

    Adicionando arquivos das atividades em java

---
+++ POO_pesoIdeal/POO_pesoIdeal.java
@@ -0,0 +1,12 @@
+//package com.mycompany.poo_pesoideal;
+
+public class POO_pesoIdeal {
+    public static void main(String[] args) {
+        
+        pesoIdeal calculaPesoIdeal = new pesoIdeal();
+        
+        calculaPesoIdeal.forneceValores();
+        calculaPesoIdeal.calcula();
+        calculaPesoIdeal.exibir();
+    }
+}
